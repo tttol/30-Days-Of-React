10042acfbb08e44923a1d94b118b9bc1ae39b9e3
t-takahashi <tkhstol.929@gmail.com>
2022-12-06 23:24:46 +0900

    do exercises

---
+++ 01_Day_JavaScript_Refresher/object/exercise-3-1.js
@@ -0,0 +1,10 @@
+console.log("============== 3-1 ==============")
+const dog = {};
+dog.name = "pochi";
+dog.legs = 4;
+dog.color = "white";
+dog.age = 5;
+dog.burk = () => "woof woof";
+console.log(dog);
+console.log(dog.burk());
+console.log("=================================")
