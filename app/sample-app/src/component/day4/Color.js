8836585fb73fbfc890c7e637873fdc07edc0668d
t-takahashi <tkhstol.929@gmail.com>
2022-12-25 08:21:41 +0900

    update Color.js(class-based component)

--- app/sample-app/src/component/day4/Color.js
+++ app/sample-app/src/component/day4/Color.js
@@ -1,14 +1,24 @@
 import './Color.css'
+import React from 'react';
 
 const getColorCodeHex = () => {
     return '#' + Math.random().toString(16).slice(2, 8);
 }
 
-function Color() {
-    const colorCode = getColorCodeHex();
-    return(
-        <div className='colorBox' style={{backgroundColor: colorCode}}>{colorCode}</div>
-    );
+// function Color() {
+//     const colorCode = getColorCodeHex();
+//     return(
+//         <div className='colorBox' style={{backgroundColor: colorCode}}>{colorCode}</div>
+//     );
+// }
+
+class Color extends React.Component {
+    render() {
+        const colorCode = getColorCodeHex();
+        return (
+            <div className='colorBox' style={{backgroundColor: colorCode}}>{colorCode}</div>
+        )
+    }
 }
 
 export default Color;
